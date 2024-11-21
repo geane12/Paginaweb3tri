c4fc11d21c0cc77abf44bea5d810ed9db269e2ac
geane12 <147194127+geane12@users.noreply.github.com>
2024-11-21 11:31:08 -0300

    s

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScrip t',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    ,
+    pergunta,
+    reposta
+)
